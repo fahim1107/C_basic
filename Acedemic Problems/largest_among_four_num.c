72e211f8efa541d097199b33a0e98e956a3f1283
fahim1107 <saiyanfaisal@gmail.com>
2022-09-28 01:36:05 +0600

    complete

--- Acedemic Problems/largest_among_four_num.c
+++ Acedemic Problems/largest_among_four_num.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a,&b,&c,&d);
+    if(a>b && a>c && a>d){
+        printf("Largest : %d",a);
+    } else if(b>a && b>c && b>d){
+        printf("Largest %d",b);
+    } else if (c>a && c>b && c>d){
+        printf("Largest : %d",c);
+    } else {
+        printf("Largest : %d",d);
+    }
+    
+
     return 0;
 }
