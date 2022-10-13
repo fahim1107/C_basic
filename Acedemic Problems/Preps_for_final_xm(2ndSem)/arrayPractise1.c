a6cc700cdc1a41084a18558c422966738ff646e9
fahim1107 <saiyanfaisal@gmail.com>
2022-10-13 16:57:07 +0600

    done

---
+++ Acedemic Problems/Preps_for_final_xm(2ndSem)/arrayPractise1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int var[10];
+    for(int i=0; i<=2;i++){
+        scanf("%d",&var[i]);
+    } for(int i=0; i<2; i++){
+        printf("%d",var[i]);
+    }
+    return 0;
+}
